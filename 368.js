643d47661113bdab9b212ab7813b0ac68d4f9317
Zach Upstone <zu213@bath.ac.uk>
2025-03-29 10:25:40 +0000

    Working version

--- 368.js
+++ 368.js
@@ -30,8 +30,8 @@ document.addEventListener("DOMContentLoaded", function () {
     document.addEventListener("mousemove", (e) => {
         if (isDragging) {
             draggable.style.position = 'fixed'
-            draggable.style.left = `${e.clientX - offsetX}px`;
-            draggable.style.top = `${e.clientY - offsetY}px`;
+            draggable.style.left = `${e.clientX}px`;
+            draggable.style.top = `${e.clientY}px`;
         }
     });
 
@@ -78,34 +78,7 @@ function checkValidPlace(e, direction, draggable){
     let element = document.elementFromPoint(e.clientX, e.clientY)
     draggable.style.display = "block";
 
-    if(direction == 1 && Number(element.id) % 6 != 0){
-        let elementAbove = document.getElementById(`${Number(element.id) - 1}`)
-        if(element.innerHTML == '' && elementAbove && elementAbove?.innerHTML == ''){
-            console.log(draggable)
-            element.appendChild(draggable.querySelector('.right')); // Move draggable to cell
-            elementAbove.appendChild(draggable.querySelector('.left'))
-            draggable.classList.remove('draggable')
-            checkMatch(Number(element.id))
-            checkMatch(Number(element.id) - 1)
-            generateChicken()
-
-        }else{
-            chickenGen.appendChild(draggable)
-        }
-    } else if(direction == 2 && Number(element.id) > 5){
-        let elementAbove = document.getElementById(`${Number(element.id) - 6}`)
-        if(element.innerHTML == '' && elementAbove && elementAbove?.innerHTML == ''){
-            element.appendChild(draggable.querySelector('.bottom')); // Move draggable to cell
-            elementAbove.appendChild(draggable.querySelector('.top'))
-            draggable.classList.remove('draggable')
-            checkMatch(Number(element.id))
-            checkMatch(Number(element.id) - 6)
-            generateChicken()
-
-        }else{
-            chickenGen.appendChild(draggable)
-        }
-    } else if(direction == 3  && Number(element.id) % 6 != 5){
+    if(direction == 2  && Number(element.id) % 6 != 5){
         let elementAbove = document.getElementById(`${Number(element.id) + 1}`)
         if(element.innerHTML == '' && elementAbove && elementAbove?.innerHTML == ''){
             element.appendChild(draggable.querySelector('.left')); // Move draggable to cell
@@ -160,19 +133,12 @@ function generateChicken(){
         let rect = element.getBoundingClientRect();
 
         // Calculate the offset of the mouse click relative to the element's position
-        offsetX = e.clientX - rect.left;   // Horizontal offset
-        offsetY = e.clientY - rect.top;  
-        console.log(element)
-        if(element.classList.contains('chickenHolder')){
+         if(element.classList.contains('chickenHolder')){
             direction = element.children[0].classList.contains('top') ? 2 : 1
         } else if(element.classList.contains('top')){
-            direction = 4
-        }else if(element.classList.contains('bottom')){
-            direction = 2
-        }else if(element.classList.contains('left')){
-            direction = 3
-        }else if(element.classList.contains('right')){
             direction = 1
+        } else if(element.classList.contains('left')){
+            direction = 2
         } else {
             throw Error('a')
         }
@@ -186,9 +152,9 @@ function generateChicken(){
 function getColour(el){
     if(el == '') return 'none'
     if(el.classList.contains('yellow')) return 'yellow'
-    if(el.classList.contains('red')) return 'yellow'
-    if(el.classList.contains('pink')) return 'yellow'
-    if(el.classList.contains('green')) return 'yellow'
+    if(el.classList.contains('red')) return 'red'
+    if(el.classList.contains('pink')) return 'pink'
+    if(el.classList.contains('green')) return 'green'
     return 'none'
 }
 
@@ -198,7 +164,7 @@ function updateChickens(){
 
 function checkMatch(pos){
     let bottomVal = pos % 6
-    var el = document.getElementById(bottomVal)
+    var el = document.getElementById(bottomVal + 12)
     var lastEl = document.getElementById(bottomVal + 6)
     var lastLastEl = document.getElementById(bottomVal)
     var val = getColour(el.children[0] ?? '')
@@ -210,29 +176,84 @@ function checkMatch(pos){
     console.log(pos)
 
     for(var i = 3; i < 7; i++){
-        console.log(val, lastVal, lastLastVal)
-
         if(val == lastVal && lastLastVal == lastVal && val != 'none'){
             counter++
             lastLastEl.innerHTML = ''
             remove = true
+        } else if((val != lastVal || lastLastVal != lastVal) && remove){
+            counter += 2
+            lastEl.innerHTML = ''
+            lastLastEl.innerHTML = ''
+            remove = false
         }
         if(i < 6){
             lastLastVal = lastVal
             lastLastEl = lastEl
             lastVal = val
             lastEl = el
-            el = document.getElementById(bottomVal + i * 6).children[0] ?? ''
-            val = getColour(el)
+            el = document.getElementById(bottomVal + i * 6)
+            val = getColour(el.children[0] ?? '')
         }
     }
 
-    if(remove){
-        counter += 2
+    if(remove) {
         lastEl.innerHTML = ''
-        el.innerHTML = ''
+        el.innerHTML =''
     }
+
+
+    bottomVal = pos - (pos % 6)
+    el = document.getElementById(bottomVal + 2)
+    lastEl = document.getElementById(bottomVal + 1)
+    lastLastEl = document.getElementById(bottomVal)
+    val = getColour(el.children[0] ?? '')
+    lastVal = getColour(lastEl.children[0] ?? '')
+    lastLastVal = getColour(lastLastEl.children[0] ?? '')
+
+    remove = false
+
+    for(var i = 3; i < 7; i++){
+        console.log(val, lastVal, lastLastVal)
+        console.log(el, lastEl, lastLastEl)
+
+        if(val == lastVal && lastLastVal == lastVal && val != 'none'){
+            counter++
+            lastLastEl.innerHTML = ''
+            remove = true
+        } else if((val != lastVal || lastLastVal != lastVal) && remove){
+            counter += 2
+            lastEl.innerHTML = ''
+            lastLastEl.innerHTML = ''
+            remove = false
+        }
+        if(i < 6){
+            lastLastVal = lastVal
+            lastLastEl = lastEl
+            lastVal = val
+            lastEl = el
+            el = document.getElementById(bottomVal + i)
+            val = getColour(el.children[0] ?? '')
+        }
+    }
+
+    if(remove) {
+        lastEl.innerHTML = ''
+        el.innerHTML =''
+    }
+
     console.log('counter:',counter)
     remainingChickens -= counter
+    updateChickens()
+}
+
+function resetBoard() {
+
+    const grid = document.getElementById('grid')
+    for(var i = 0; i < 6; i++){
+        for(var j = 0; j<6; j++){
+            grid.children[i].children[j].children[0].innerHTML = ''
+        }
+    }
+    remainingChickens = 368
     updateChickens()
 }
